0cf514941a8b61b07dae34f33a09620ce2f592da
Online Application Support <jem@support-01.(none)>
2009-11-26 09:05:15 +1100

    Added repo w 0.1.2 & removed boot lib

--- project/build/src/ScweeryProject.scala
+++ project/build/src/ScweeryProject.scala
@@ -8,7 +8,7 @@ class ScweeryProject(info: ProjectInfo) extends DefaultProject(info) with AutoCo
   val specs = "org.scala-tools.testing" % "specs" % "1.5.0" % "test->default"
   val mockito = "org.mockito" % "mockito-core" % "1.7" % "test->default"
 
-  val sxr = compilerPlugin("org.scala-tools.sxr" % "sxr_2.7.4" % "0.2.3")
+  val sxr = compilerPlugin("org.scala-tools.sxr" % "sxr_2.7.5" % "0.2.3")
 
   override def compileOptions =
     CompileOption("-P:sxr:base-directory:" + mainScalaSourcePath.asFile.getAbsolutePath) ::
